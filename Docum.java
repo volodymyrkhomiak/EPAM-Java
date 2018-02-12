4fed5d5ef8ff60793159a799ae176c965d08e7b8
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-02-12 19:09:09 +0200

    Add files via upload

---
+++ Docum.java
@@ -0,0 +1,9 @@
+
+public class Docum {
+		public static void main(String[] args) {
+		System.out.println("Hello World");
+		/** Entry poing to class & application.
+		* @throws exceptions No exceptions thrown
+		*/
+}/*output text*/
+}
