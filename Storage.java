047c8285fa0ac1281af61a4d30d984f98f7d9a2e
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-02-12 19:08:01 +0200

    Add files via upload

---
+++ Storage.java
@@ -0,0 +1,15 @@
+package lab6;
+
+public class Storage {	
+	public static void main(String[] args) {
+{
+	class Store{
+		int storage(String s) {
+			return s.length()*2;
+		}
+	}
+	Store a=new Store();
+	System.out.println(a.storage("Hello!!!"));
+}
+}
+}
