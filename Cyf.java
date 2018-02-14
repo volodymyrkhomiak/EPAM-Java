cfc2ce434854a0f84420f05a340e5301b85eade0
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-02-14 20:18:29 +0200

    Add files via upload

---
+++ Cyf.java
@@ -0,0 +1,13 @@
+package primes;
+
+public class Cyf{
+	public static void main(String[] args) {
+		for(int i = 1; i < 1000; i++ ) {
+			int factors = 0;
+			for(int j = 1; j < (i + 2)/2; j++ ) {
+				if((i % j) == 0) factors++; 			
+			}
+			if(factors < 2) System.out.println(i + " ha");
+		}
+	}
+}
