d87eb1f532a3ecdf3f092f9f1a03ae063dce3b27
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-03-05 18:39:08 +0200

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args){
+        Double a  = Double.MAX_VALUE;
+        Float b = Float.MAX_VALUE;
+
+        System.out.println("Max double - "  + a);
+        System.out.println("Max float - " + b);
+
+    }
+}
