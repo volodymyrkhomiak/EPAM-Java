56ce722b014d952be4b42d9d2271ac2613566112
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-03-05 18:38:27 +0200

    Add files via upload

---
+++ CompareForever.java
@@ -0,0 +1,26 @@
+import static net.mindview.util.Print.*;
+import java.util.*;
+
+public class CompareForever {
+	public static void main(String[] args) {
+		Random rand1 = new Random();
+		Random rand2 = new Random();
+		for(int i = 0; i < 25; i++) {
+			int x = rand1.nextInt();
+			int y = rand2.nextInt();
+			if(x < y) print(x + " < " + y);
+			else if(x > y) print(x + " > " + y);
+			else print(x + " = " + y);
+		}
+		Random rand3 = new Random();
+		Random rand4 = new Random();
+		while(true) {
+		//for(int i = 0; i < 25; i++) {
+			int x = rand3.nextInt(10);
+			int y = rand4.nextInt(10);
+			if(x < y) print(x + " < " + y);
+			else if(x > y) print(x + " > " + y);
+			else print(x + " = " + y);
+		}
+	}
+}
